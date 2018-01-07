ffd8525f57c50eac44f1b461dfcd56a2b9f28f59
Alexis Barltrop <alexisbarltrop@gmail.com>
2018-01-08 12:12:41 +1300

    Made branch for Alexis

--- Etude1_Rollin/MyRollin.java
+++ Etude1_Rollin/MyRollin.java
@@ -1,13 +1,19 @@
 package rollin;
+import java.util.*;
 
 public class MyRollin extends Rollin {
 
+    private static final Random rand = new Random();
+    
+    
     public MyRollin(int[] dice){
         super(dice);
     }
 
     public int handleRoll(int roll){
-        return 0;
+        int index = rand.nextInt(6);
+        dice[index] = roll;
+        return index;
     }
 
 } 
--- Etude1_Rollin/RollinApp.java
+++ Etude1_Rollin/RollinApp.java
@@ -2,11 +2,42 @@ package rollin;
 import java.util.*;
 public class RollinApp{
 
+    private static final Random rand = new Random();
+    private static final int handSize = 6;
+    private static final int diceSize = 6;
+    
     public static void main (String[] args){
         /** Code for running and Testing here*/
-        int[] myDice ={1,2,3,4,5,6};
+        int[] myDice = createStartDice();
         MyRollin myRoll = new MyRollin(myDice);
+        int numberOfTurns = 0;
+        int numberOfSwaps = 0;
         System.out.println(Arrays.toString(myRoll.getDice()));
+        System.out.println(myRoll.isComplete());
+        while(!myRoll.isComplete()){
+            int newRoll = rollDice();
+            myRoll.handleRoll(newRoll);
+            System.out.println(Arrays.toString(myRoll.getDice()));
+            numberOfTurns++;
+        }
+        System.out.println(Arrays.toString(myRoll.getDice()));
+        System.out.println(myRoll.isComplete());
+        System.out.println("Number of Turns : " +numberOfTurns);
+        
+    }
+
+    public static int[] createStartDice(){
+        int[] diceHand= new int[handSize];
+        for(int i = 0;i<handSize;i++){
+            diceHand[i] = rand.nextInt(diceSize);
+        }
+        return diceHand;
+    }
+
+    public static int rollDice(){
+        int roll = rand.nextInt(diceSize);
+        //System.out.println(roll);
+        return roll;
     }
 }
 
