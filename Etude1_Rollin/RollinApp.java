443e20d4892c4fdbbdbf94d17b59ce4bc18cf5c6
Anthony Dickson <eight0153@gmail.com>
2018-01-08 13:35:20 +1300

    Generate dice with Random instance

--- Etude1_Rollin/RollinApp.java
+++ Etude1_Rollin/RollinApp.java
@@ -6,24 +6,30 @@ public class RollinApp{
 
     public static void main (String[] args){
         /** Code for running and Testing here*/
-        // Create random instance and seed it for testing
+        // Create random instance and seed it for testing.
         Random r = new Random(2018);
 
-        // Initialise MyRollin instance with two sets, { 1, 2, 3 } and { 4, 4, 4 }
-        int[] myDice = { 1, 3, 4, 4, 2, 4 };
-        MyRollin myRoll = new MyRollin(myDice);
-        System.out.println(Arrays.toString(myRoll.getDice()));
-
-        int i = 0; // Keep track of how many rolls it takes to get two sets.
-
-        // Roll until we get two sets
-        while (!myRoll.isComplete()) {
-            myRoll.handleRoll(r.nextInt(6) + 1);
-            i++;
+        // Create an array of six random dice rolls.
+        int[] initialDice = new int[6];
+        for (int i = 0; i < 6; i++) {
+            initialDice[i] = r.nextInt(6) + 1;
         }
 
+        // Initialise MyRollin instance and print initial dice.
+        MyRollin myRollin = new MyRollin(initialDice);
+        System.out.println(Arrays.toString(myRollin.getDice()));
+
+        int i = 0; // Used to keep track of how many rolls it takes to get two sets.
+
+        // // Roll until we get two sets.
+        // while (!myRollin.isComplete()) {
+        //     myRollin.handleRoll(r.nextInt(6) + 1);
+        //     i++;
+        // }
+        
+        // Print out how many rolls it took and the final dice.
         System.out.println("Finished in " + i + " rolls.");
-        System.out.println(Arrays.toString(myRoll.getDice()));
+        System.out.println(Arrays.toString(myRollin.getDice()));
     }
 }
 
