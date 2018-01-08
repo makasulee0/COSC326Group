5d3e30e8c2fa98621317b361323a0bd865fcfb03
Anthony Dickson <eight0153@gmail.com>
2018-01-08 15:23:20 +1300

    handleRoll() just replaces a random die

--- Etude1_Rollin/MyRollin.java
+++ Etude1_Rollin/MyRollin.java
@@ -1,13 +1,47 @@
 package rollin;
 
+import java.util.*;
+
+/**
+ * Implementation of the abstract class Rollin.
+ * <see>Rollin</see>
+ */
 public class MyRollin extends Rollin {
+    /** Return value for handleRoll() if no dice are replaced. */
+    public static final int NO_REPLACE = -1; 
+    public static final int[] NOT_FOUND = { -1, -1, -1 };
+    public static final Random r = new Random(2018);
 
     public MyRollin(int[] dice){
         super(dice);
     }
 
+    /**
+     * Replace the die at the given index with given value.
+     * @param i The index of the die to replace.
+     * @param value The new value of the die. 
+     */
+     public void replace(int i, int value) {
+         dice[i] = value;
+     }
+
+    /**
+     * Given a die roll, determine whether or not to replace a dice and which
+     * one to replace.
+     *
+     * @param roll The value of the die roll.
+     * @return The index of the die whose value will be replaced by the roll, or
+     * any int outside of 0 to 5 if no replacement is made.
+     */
     public int handleRoll(int roll){
-        return 0;
+        // If we do not have two sets already...
+        if (!isComplete()) {
+            // Replace a random die.
+            return r.nextInt(6);
+        }
+
+        // Return value indicating that no dice were replaced.
+        return NO_REPLACE;
     }
 
 } 
--- Etude1_Rollin/RollinApp.java
+++ Etude1_Rollin/RollinApp.java
@@ -1,3 +1,7 @@
+/**
+ * RollinApp.java
+ */
+
 package rollin;
 
 import java.util.*;
@@ -7,7 +11,7 @@ public class RollinApp{
     public static void main (String[] args){
         /** Code for running and Testing here*/
         // Create random instance and seed it for testing.
-        Random r = new Random(2018);
+        Random r = new Random(); // new Random(2018);
 
         // Create an array of six random dice rolls.
         int[] initialDice = new int[6];
@@ -17,19 +21,27 @@ public class RollinApp{
 
         // Initialise MyRollin instance and print initial dice.
         MyRollin myRollin = new MyRollin(initialDice);
-        System.out.println(Arrays.toString(myRollin.getDice()));
+        System.out.println("Initial dice: " + Arrays.toString(myRollin.getDice()));
 
-        int i = 0; // Used to keep track of how many rolls it takes to get two sets.
+        int i = 0; // How many rolls it took to get two sets.
+        int replace; // Which die to replace or value indicating no replacement.
+        int roll; // The value of the die from the die roll.
 
-        // // Roll until we get two sets.
-        // while (!myRollin.isComplete()) {
-        //     myRollin.handleRoll(r.nextInt(6) + 1);
-        //     i++;
-        // }
+        // Roll until we get two sets.
+        while (!myRollin.isComplete() && i < 100) {
+            roll = r.nextInt(6) + 1;
+            replace = myRollin.handleRoll(roll);
+
+            if (replace != MyRollin.NO_REPLACE) {
+                myRollin.replace(replace, roll);
+            }
+
+            i++;
+        }
         
         // Print out how many rolls it took and the final dice.
         System.out.println("Finished in " + i + " rolls.");
-        System.out.println(Arrays.toString(myRollin.getDice()));
+        System.out.println("Final dice: " + Arrays.toString(myRollin.getDice()));
     }
 }
 
